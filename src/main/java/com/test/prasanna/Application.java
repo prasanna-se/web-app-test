06084466e8094b10d578e421cf612ed059dab0bc
PrasannaSR <prasanna.padmanaban1@engineering.digital.dwp.gov.uk>
2021-02-19 11:16:26 +0000

    created maven framework

---
+++ src/main/java/com/test/prasanna/Application.java
@@ -0,0 +1,8 @@
+package com.test.prasanna;
+
+public class Application {
+    public static void main( String[] args )
+    {
+        System.out.println( "Behaviour Driven Development" );
+    }
+}
