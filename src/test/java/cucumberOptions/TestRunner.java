0f35168e8acf9cc17d85bd74fc9a24e874bb2374
PrasannaSR <prasanna.padmanaban1@engineering.digital.dwp.gov.uk>
2021-02-19 20:20:09 +0000

    changes in runner

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -1,4 +1,15 @@
 package cucumberOptions;
 
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = "stepDefinitions",tags="@WebTest", strict = true, monochrome = true,
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json", "junit:target/cukes.xml"})
+
 public class TestRunner {
 }
