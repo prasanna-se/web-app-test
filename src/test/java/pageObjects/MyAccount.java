953621cfd6f90da9cc11010a619d47dfed5883ad
PrasannaSR <prasanna.padmanaban1@engineering.digital.dwp.gov.uk>
2021-02-19 18:49:29 +0000

    added checkout scenario

---
+++ src/test/java/pageObjects/MyAccount.java
@@ -0,0 +1,27 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage {
+
+    public WebDriver driver;
+
+    @FindBy(id = "email")
+    WebElement emailAddress;
+
+    @FindBy(id = "passwd")
+    WebElement password;
+
+    @FindBy(id = "SubmitLogin")
+    WebElement signIn;
+
+
+    public void login(String email, String pass) {
+        emailAddress.sendKeys(email);
+        password.sendKeys(pass);
+        signIn.click();
+    }
+
+}
